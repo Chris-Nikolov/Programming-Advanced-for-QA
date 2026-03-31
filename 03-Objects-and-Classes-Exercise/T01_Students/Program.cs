e6bbd01704cb5c85ac32cfa9495640de594ef7db
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-03-31 19:53:23 +0300

    add: ObjectsClassesExercise

---
+++ 03-Objects-and-Classes-Exercise/T01_Students/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace T01_Stidents;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>();
+        
+       var n = int.Parse(Console.ReadLine());
+
+       for (var i = 0; i < n; i++)
+       {
+           var studentInfo = Console.ReadLine().Split().ToArray();
+           
+           var firstName = studentInfo[0];
+           var lastName = studentInfo[1];
+           var grade = double.Parse(studentInfo[2]);
+           
+           Student currentStudent = new Student();
+           currentStudent.FirstName = firstName;
+           currentStudent.LastName = lastName;
+           currentStudent.Grade = grade;
+           students.Add(currentStudent);
+       }
+       
+       foreach (var student in students.OrderByDescending(x => x.Grade))
+       {
+           Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");}
+    }
+    
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+        
+    }
+}
---
+++ 03-Objects-and-Classes-Exercise/T02_Articles/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace T02_Articles;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        var articleInfo = input.Split(", ").ToArray();
+        var title = articleInfo[0];
+        var content = articleInfo[1];
+        var author = articleInfo[2];
+        
+        Article article = new Article(title, content, author);
+        
+        var n = int.Parse(Console.ReadLine());
+
+        for (var i = 0; i < n; i++)
+        {
+            var arr = Console.ReadLine().Split(": ").ToArray();
+            
+            var command = arr[0];
+            var token = arr[1];
+            switch (command)
+            {
+                case "Edit":
+                    article.Edit(token);
+                    break;
+                case "ChangeAuthor":
+                    article.ChangeAuthor(token);
+                    break;
+                case "Rename":
+                    article.Rename(token);
+                    break;
+            }
+            
+        }
+        Console.WriteLine(article.ToString());
+    }
+}
+
+class Article
+{
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public  string Author { get; set; }
+
+    public void Edit(string newContent)
+    {
+        Content = newContent;
+    }
+
+    public void ChangeAuthor(string newAuthor)
+    {
+        Author = newAuthor;
+    }
+
+    public void Rename(string newTitle)
+    {
+        Title = newTitle;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
